af4224bb885001f2f5577350f41575e3aa9fbb7f
Σίγμα <sigma@ingotsoftware.com>
2022-04-05 20:52:48 -0500

    nvim vue lsp, docker aliases

--- nvim/lua/lsp/vue-ls.lua
+++ nvim/lua/lsp/vue-ls.lua
@@ -1,4 +1,4 @@
-local server_path = LSP_SERVERS_PATH .. "/volar/node_modules/@volar/server/bin/volar-server.js"
+local server_path = LSP_SERVERS_PATH .. "/volar/node_modules/@volar/vue-language-server/bin/vue-language-server.js"
 
 require("lspconfig").volar.setup {
   cmd = {server_path, "--stdio"},
