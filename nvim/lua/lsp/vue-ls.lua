24d7ae9b84b7eb3d56dbc95a5ba0244135f5ad0d
Σίγμα <sigma@ingotsoftware.com>
2022-11-13 14:28:30 -0500

    lsp update

--- nvim/lua/lsp/vue-ls.lua
+++ nvim/lua/lsp/vue-ls.lua
@@ -1,6 +1,17 @@
-local server_path = LSP_SERVERS_PATH .. "/volar/node_modules/@volar/vue-language-server/bin/vue-language-server.js"
+local server_root = LSP_SERVERS_PATH .. "/volar/node_modules"
+local vue_server_path = server_root .. "/@volar/vue-language-server/bin/vue-language-server.js"
+local ts_server_path = server_root .. "/typescript/lib/tsserverlibrary.js"
+local root_pattern = require("lspconfig/util").root_pattern
 
 require("lspconfig").volar.setup {
-  cmd = {server_path, "--stdio"},
-  filetypes = {'vue'}
+  cmd = {"node", vue_server_path, "--stdio"},
+  filetypes = {'vue'},
+  root_dir = root_pattern("package.json","vue.config.js"),
+  init_options = {
+    hostInfo = "neovim",
+    typescript = {
+      serverPath = ts_server_path
+    }
+  }
 }
+
