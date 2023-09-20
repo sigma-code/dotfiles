c5529e23baa6dd1898e215392abd22c6d32453a7
Pablo <pablo@gyarados.wolfseden.net>
2023-09-20 09:17:27 -0500

    debian change

--- zsh/p10k.zsh
+++ zsh/p10k.zsh
@@ -185,7 +185,7 @@
 
   #################################[ os_icon: os identifier ]##################################
   # OS identifier color.
-  typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=33
+  typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=6
   # Custom icon.
   # typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION='⭐'
 
